2748e234dba2c476ccc12fa109d66946c3717481
jp112sdl <jp112sdl@icloud.com>
2018-03-07 12:03:07 +0100

    added more executable option to files

--- Contrib/CCURM/build.sh
+++ Contrib/CCURM/build.sh
@@ -2,5 +2,8 @@
 rm HB-UW-Sen-THPL_CCURM-addon.tgz
 cd HB-UW-Sen-THPL_CCURM-addon-src
 chmod +x update_script
+chmod +x addon/install*
+chmod +x addon/update-check.cgi
+chmod +x rc.d/*
 tar -zcvf ../HB-UW-Sen-THPL_CCURM-addon.tgz *
 cd ..
